6d5d14c3683415a4e2b53a143bf3671457c60d1e
Desi <d_andreeva82@yahoo.co.uk>
2023-09-22 21:56:14 +0100

    add

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/11. Animal Types.cs
@@ -0,0 +1,22 @@
+﻿namespace _11._Animal_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = (Console.ReadLine());
+            if (input == "dog")
+            {
+                Console.WriteLine("mammal");
+            }
+            else if (input == "crocodile" || input == "tortoise" || input == "snake")
+            {
+                Console.WriteLine("reptile");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
