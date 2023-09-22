b6a96c5f1e850540dd4d7e8192aef99dfd2f7805
Desi <d_andreeva82@yahoo.co.uk>
2023-09-22 21:37:08 +0100

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/Exercise/01. Exercise/14. Journey.cs
@@ -0,0 +1,61 @@
+﻿namespace _05._Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            string accommodation = "";
+            double cost = 0.00;
+
+            if(season == "summer")
+            {
+                if(budget <= 100)
+                {
+                    cost = budget * 0.3;
+                    destination = "Bulgaria";
+                    accommodation = "Camp";
+                }
+                else if(budget > 100 && budget <= 1000)
+                {
+                    cost = budget * 0.4;
+                    destination = "Balkans";
+                    accommodation = "Camp";
+                }
+                else if (budget > 1000)
+                {
+                    cost = budget * 0.9;
+                    destination = "Europe";
+                    accommodation = "Camp";
+                }
+            }
+            else if(season == "winter")
+            {
+
+                if (budget <= 100)
+                {
+                    cost = budget * 0.7;
+                    destination = "Bulgaria";
+                    accommodation = "Hotel";
+                }
+                else if (budget > 100 && budget <= 1000)
+                {
+                    cost = budget * 0.8;
+                    destination = "Balkans";
+                    accommodation = "Hotel";
+                }
+                else if (budget > 1000)
+                {
+                    cost = budget * 0.9;
+                    destination = "Europe";
+                    accommodation = "Hotel";
+                }
+            }
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{accommodation} - {cost:f2}");
+        }
+    }
+}
