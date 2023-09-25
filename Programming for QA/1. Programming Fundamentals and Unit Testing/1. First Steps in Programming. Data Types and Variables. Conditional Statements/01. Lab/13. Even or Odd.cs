56987853f0ec1fcb02049ad624d10adf4c8c2047
Desi <d_andreeva82@yahoo.co.uk>
2023-09-25 23:14:27 +0100

    add file

--- Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/13. Even or Odd.cs
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/13. Even or Odd.cs
@@ -1,1 +1,18 @@
-﻿
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
