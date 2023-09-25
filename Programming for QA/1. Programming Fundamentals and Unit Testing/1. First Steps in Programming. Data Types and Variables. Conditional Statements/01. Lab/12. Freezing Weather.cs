235b217eaa94ef6caedec5f396409708e7955931
Desi <d_andreeva82@yahoo.co.uk>
2023-09-25 22:45:37 +0100

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/12. Freezing Weather.cs
@@ -0,0 +1,18 @@
+﻿namespace _12. Freazing Weather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperature = double.Parse(Console.ReadLine());
+            if (temperature < 0)
+            {
+                Console.WriteLine("Freazing weather!");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}   }
