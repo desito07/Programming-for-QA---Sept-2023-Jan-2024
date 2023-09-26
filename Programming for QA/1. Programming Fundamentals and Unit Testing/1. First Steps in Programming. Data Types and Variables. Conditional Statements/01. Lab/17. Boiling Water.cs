90cb395aca9fe6a84550cd38e9d7b89b9a12458e
Desi <d_andreeva82@yahoo.co.uk>
2023-09-26 23:33:08 +0100

    add

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/17. Boiling Water.cs
@@ -0,0 +1,19 @@
+﻿namespace Boiling_Water
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            if(num > 100)
+            {
+                Console.WriteLine($"The water is boiling");
+            }
+            else
+            {
+                Console.WriteLine("The water is not hot enough");
+            }
+        }
+    }
+}
