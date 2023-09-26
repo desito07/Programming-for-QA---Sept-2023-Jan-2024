ec8756d7e55f40fe4a85852fc0ff584e4f765072
Desi <d_andreeva82@yahoo.co.uk>
2023-09-26 23:33:15 +0100

    add

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/19. Ticket Price.cs
@@ -0,0 +1,26 @@
+﻿namespace Ticket_Price
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ticketType = Console.ReadLine();
+            double price = 0.00;
+
+            if(ticketType == "student")
+            {
+                price += 1.00;
+                Console.WriteLine($"${price:f2}");
+            }
+            else if(ticketType == "regular")
+            {
+                price += 1.60;
+                Console.WriteLine($"${price:f2}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid ticket type!");
+            }
+        }
+    }
+}
---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/20. Area of Figures.cs
@@ -0,0 +1,37 @@
+﻿namespace Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double area = 0.00;
+
+
+            if (figure == "square")
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                area = num1 * num1;
+                Console.WriteLine($"{area:f2}");
+            }
+            else if (figure == "rectangle")
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine());
+                area = num1 * num2;
+                Console.WriteLine($"{area:f2}");
+            }
+            else if (figure == "circle")
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                area = Math.PI * num1 * num1;
+                Console.WriteLine($"{area:f2}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid figure.");
+            }
+            
+        }
+    }
+}
