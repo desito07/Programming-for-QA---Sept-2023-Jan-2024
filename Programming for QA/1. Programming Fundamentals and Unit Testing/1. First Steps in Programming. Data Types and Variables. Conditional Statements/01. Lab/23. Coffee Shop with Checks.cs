1c6c19b6dfb9bf575d6f6368928ae4097b608a06
Desi <d_andreeva82@yahoo.co.uk>
2023-09-26 23:33:36 +0100

    add

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/23. Coffee Shop with Checks.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Coffee_Shop_with_Checks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string extra = Console.ReadLine();
+            double price = 0.0;
+            bool validOrder = true;
+
+            if (drink == "tea")
+            {
+                price = 0.6;
+            }
+            else if (drink == "coffee")
+            {
+                price = 1;
+            }
+            else
+            {
+                Console.WriteLine("Unknown drink");
+                validOrder = false;
+            }
+
+            if(validOrder)
+            {
+                if (extra == "sugar")
+                {
+                    price += 0.4;
+                }
+                else if (extra == "no")
+                {
+                    price += 0.0;
+                }
+                else
+                {
+                    Console.WriteLine("Unknown extra");
+                    validOrder = false;
+                }
+            }
+
+            if (validOrder)
+            {
+                Console.WriteLine($"Final price: ${price:f2}");
+            }
+ 
+        }
+    }
+}
