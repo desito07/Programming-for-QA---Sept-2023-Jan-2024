b99151f0e340a9162083552666bd4daf162a232c
Desi <d_andreeva82@yahoo.co.uk>
2023-09-26 23:33:21 +0100

    add

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/21. Valid Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Valid_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int side1 = int.Parse(Console.ReadLine());
+            int side2 = int.Parse(Console.ReadLine());
+            int side3 = int.Parse(Console.ReadLine());
+
+            bool isValidTriangle = true;
+
+            if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+            {
+                isValidTriangle = false;
+                Console.WriteLine("Invalid Triangle");
+            }
+            else
+            {
+                isValidTriangle = true;
+                Console.WriteLine("Valid Triangle");
+            }
+        }
+    }
+}
---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Lab/22. Coffee Shop.cs
@@ -0,0 +1,28 @@
+﻿namespace Coffee_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string extra = Console.ReadLine();
+            double price = 0.0;
+
+            if(drink == "tea")
+            {
+                price = 0.6;
+            }
+            else if(drink == "coffee")
+            {
+                price = 1;
+            }
+
+            if(extra == "sugar")
+            {
+                price += 0.4;
+            }
+            Console.WriteLine($"Final price: ${price:f2}");
+
+        }
+    }
+}
