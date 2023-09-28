f13e51a0d0cdab6ac6f33fd3e0c33bae8c7344d1
Desi <d_andreeva82@yahoo.co.uk>
2023-09-28 22:26:49 +0100

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Exercise/19. Number Type.cs
@@ -0,0 +1,23 @@
+﻿namespace _19._Number_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if(num < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else if(num > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("zero");
+            }
+        }
+    }
+}
---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Exercise/20. ATM.cs
@@ -0,0 +1,26 @@
+﻿namespace _20._ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int balance = int.Parse(Console.ReadLine());
+            int withdraw = int.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+
+            if(balance > withdraw && withdraw < limit)
+            {
+                Console.WriteLine("The withdraw was successful.");
+            }
+            else if(withdraw > balance && withdraw < limit)
+            {
+                Console.WriteLine("Insufficient availability.");
+            }
+            else if(withdraw > balance && withdraw > limit)
+            {
+                Console.WriteLine("The limit was exceeded.");
+            }
+
+        }
+    }
+}
