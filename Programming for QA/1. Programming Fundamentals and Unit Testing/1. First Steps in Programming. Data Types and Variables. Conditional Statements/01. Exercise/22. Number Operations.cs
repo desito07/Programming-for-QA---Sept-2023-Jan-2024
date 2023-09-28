04bbdc9cf763d8d4d7e1fdedba8cc097b8d46032
Desi <d_andreeva82@yahoo.co.uk>
2023-09-28 22:53:09 +0100

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Exercise/22. Number Operations.cs
@@ -0,0 +1,32 @@
+﻿namespace _22._Number_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            string operatorN = Console.ReadLine();
+            double result = 0.0;
+
+            if(operatorN == "+")
+            {
+                result = n1 + n2;
+            }
+            else if(operatorN == "-")
+            {
+                result = n1 - n2;
+            }
+            else if (operatorN == "*")
+            {
+                result = n1 * n2;
+            }
+            else if( operatorN == "/")
+            {
+                result = n1 / n2;
+            }
+            Console.WriteLine($"{n1} {operatorN} {n2} = {result:f2}");
+
+        }
+    }
+}
---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/1. First Steps in Programming. Data Types and Variables. Conditional Statements/01. Exercise/25. Working Hours.cs
@@ -0,0 +1,20 @@
+﻿namespace _25._Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if((num >= 10 && num <= 18) && (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday"))
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
