c05777157256341c09fe8b7db3b47068afc5581f
Desi <d_andreeva82@yahoo.co.uk>
2023-10-15 22:27:06 +0100

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/3. Nested Loops and Methods/03. Lab/15. Repeat String.cs
@@ -0,0 +1,24 @@
+﻿static void PrintLinе(int start, int end)
+{
+    for(int i = start; i <= end; i++)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+
+static void PrintTriangle(int num)
+{
+    for(int j = 1; j<= num; j++)
+    {
+        PrintLinе(1, j);
+    }
+    for(int j = num - 1; j >= 1; j--)
+    {
+        PrintLinе(1, j);
+    }
+    
+}
+
+int number = int.Parse(Console.ReadLine());
+PrintTriangle(number);
