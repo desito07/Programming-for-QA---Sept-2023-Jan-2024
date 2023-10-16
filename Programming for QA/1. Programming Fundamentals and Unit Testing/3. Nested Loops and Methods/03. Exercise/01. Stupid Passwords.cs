a7dd945794f6f8e8e864ef995c00fe8ae572fb51
Desi <d_andreeva82@yahoo.co.uk>
2023-10-16 14:00:49 +0100

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/3. Nested Loops and Methods/03. Exercise/01. Stupid Passwords.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Runtime.ExceptionServices;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int firstPart = 2; firstPart <= n; firstPart+= 2)
+{
+    for (int secondPart = 1;  secondPart <= n; secondPart+= 2)
+    {
+        int result = firstPart * secondPart;
+        Console.Write($"{firstPart}{secondPart}{result} ");
+    }
+}
---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/3. Nested Loops and Methods/03. Exercise/05. Happy Numbers.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+bool isHappy = true;
+
+for (int i = 1; i <= 9; i++)
+{
+    for (int j = 0; j <= 9; j++)
+    {
+        for (int k = 0; k <=9; k++)
+        {
+            for (int d = 0; d <= 9; d++)
+            {
+                if (i+j==n && k+d == n)
+                {
+                    Console.Write($"{i}{j}{k}{d} ");
+
+                }
+            }
+        }
+    }
+}
+Console.WriteLine();
