44b61816fc2d37118d881963929df5d284e343ef
Desi <d_andreeva82@yahoo.co.uk>
2023-10-29 23:08:20 +0000

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/5. Unit Testing - Methods, Arrays and Lists/05. Lab/Resources - Unit-Testing-Methods/UnitTests/StringReverseTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = "";
+
+        // Act
+        string actual = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("", actual);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        // Arrange
+        string input = "s";
+
+        // Act
+        string actual = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("s", actual);
+        Assert.AreEqual(1, actual.Length);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        // Arrange
+        string input = "test";
+
+        // Act
+        string actual = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("tset", actual);
+        Assert.AreEqual(input.Length, actual.Length);
+    }
+}
