af2044bc1b676f3a300055f85f052db73efea3d9
Desi <d_andreeva82@yahoo.co.uk>
2023-10-31 23:54:32 +0000

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/6. Exam Prep/ExPrep1/01. Sum Factorial Even Digits.cs
@@ -0,0 +1,41 @@
+﻿
+
+// Read a number from the console and analyse digits
+// Calculate Factorial using a method
+// Divide the number by % 10 and / 10
+// if even check 
+
+//int result = CalculateFactorial(4);
+//Console.WriteLine(result);
+
+int result = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (result != 0)
+{
+    int digit = result % 10;
+
+    if(digit % 2 == 0)
+    {
+        int resultFactorial = CalculateFactorial(digit);
+        sum += resultFactorial;
+    }
+    //Console.WriteLine(digit);
+    result = result / 10;
+}
+
+Console.WriteLine(sum);
+static int CalculateFactorial(int num)
+{
+    int result = 1;
+
+    while (num > 0)
+    {
+        result = result * num;
+        num -= 1;
+    }
+
+    return result;
+}
+
+
---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/6. Exam Prep/ExPrep1/02. Middle Elements.cs
@@ -0,0 +1,27 @@
+﻿// Read an array of integers
+
+int [] array = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+//Find the middle numbers
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    int current = array[i];
+}
+
+int middleRightElementIndex = array.Length / 2;
+int middleLeftElementIndex = middleRightElementIndex - 1;
+double avr = (array[middleRightElementIndex] + array[middleLeftElementIndex]) / 2.0;
+Console.WriteLine($"{avr:f2}");
+
+//Console.WriteLine(array[middleRightElementIndex]);
+//Console.WriteLine(middleLeftElementIndex);
+
+
+
+
+
+
+
