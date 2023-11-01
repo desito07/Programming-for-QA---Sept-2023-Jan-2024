56dee318f511e96be6398c5264b0affe6528be11
Desi <d_andreeva82@yahoo.co.uk>
2023-11-01 22:02:57 +0000

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/6. Exam Prep/ExPrep2/01. Magic Numbers.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+int num = int.Parse(Console.ReadLine());
+bool isThereNoMatchingNumbers = true;
+
+
+for (int i = 1; i <= num; i++)
+{
+    int current = i;
+    int sum = 0;
+    bool isAllDigitsPrime = true;
+
+    while (current > 0)
+    {
+        int digit = current % 10;
+        current = current / 10;
+        bool isDigitPrime = digit == 2 || digit == 3 || digit == 5 || digit == 7;
+
+        if (isDigitPrime)
+        {
+            sum += digit;
+        }
+        else
+        {
+            isAllDigitsPrime = false;
+            break;
+        }
+    }
+    if (isAllDigitsPrime && sum % 2 == 0)
+    {
+        Console.Write(i + " ");
+        isThereNoMatchingNumbers = false;
+    }
+}
+if (isThereNoMatchingNumbers)
+{
+        Console.WriteLine("no");
+}
+
+
---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/6. Exam Prep/ExPrep2/02. Min  Max Values.cs
@@ -0,0 +1,24 @@
+﻿int[] array = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+int n = int.Parse(Console.ReadLine());
+int max = int.MinValue;
+int min = int.MaxValue;
+
+for (int i = 0; i < n; i++)
+{
+    int current = array[i];
+    if(current > max)
+    {
+        max = current;
+    }
+    if(current < min)
+    {
+        min = current;
+    }
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
+
