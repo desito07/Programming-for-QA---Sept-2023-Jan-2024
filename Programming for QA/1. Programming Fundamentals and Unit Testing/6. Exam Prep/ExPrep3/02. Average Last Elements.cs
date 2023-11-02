4500c77aa23982db15eb5f5ef062f5e25cee4c6e
Desi <d_andreeva82@yahoo.co.uk>
2023-11-02 22:35:39 +0000

    add file

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/6. Exam Prep/ExPrep3/02. Average Last Elements.cs
@@ -0,0 +1,16 @@
+﻿
+int[] array = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+int n = int.Parse(Console.ReadLine());
+double sum = 0.0;
+for (int i = array.Length - 1; i > array.Length - 1 - n; i--)
+{
+    int current = array[i];
+    sum += current;
+   
+}
+double avr = sum / n;
+Console.Write(avr);
