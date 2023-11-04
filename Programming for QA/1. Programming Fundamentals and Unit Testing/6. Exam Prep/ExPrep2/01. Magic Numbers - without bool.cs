c2d9bc8c4ecb548ca17a72b28978a867bb82f118
Desi <d_andreeva82@yahoo.co.uk>
2023-11-04 22:58:20 +0000

    Create 01. Magic Numbers - without bool.cs

---
+++ Programming for QA/1. Programming Fundamentals and Unit Testing/6. Exam Prep/ExPrep2/01. Magic Numbers - without bool.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int num = int.Parse(Console.ReadLine());
+int totalCountOfMagicNumbers = 0;
+
+
+for (int i = 1; i <= num; i++)
+{
+    int current = i;
+    int sum = 0;
+    int totalNumberOfDigits = 0;
+    int totalNumberOfPrimeDigits = 0;
+
+    while (current > 0)
+    {
+        int digit = current % 10;
+        current = current / 10;
+        totalNumberOfDigits++;
+        
+        if (digit == 2 || digit == 3 || digit == 5 || digit == 7)
+        {
+            sum += digit;
+            totalNumberOfPrimeDigits++;
+        }
+      
+    }
+    if (totalNumberOfDigits == totalNumberOfPrimeDigits && sum % 2 == 0)
+    {
+        Console.Write(i + " ");
+        totalCountOfMagicNumbers++;
+    }
+}
+if (totalCountOfMagicNumbers == 0)
+{
+    Console.WriteLine("no");
+}
+
+
