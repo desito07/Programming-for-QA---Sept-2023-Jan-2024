f72f7e72e44a6f512e291d31994a83d94b48ddc1
Desi <d_andreeva82@yahoo.co.uk>
2023-11-22 22:56:58 +0000

    add file

---
+++ Programming for QA/2. Programming Advanced for QA/3. Objects and Classes and Definning Classes/03. Lab/01. Car/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car vwMk3 = new Car();
+
+            vwMk3.Make = "VW";
+            vwMk3.Model = "MK3";
+            vwMk3.Year = 1992;
+
+            Console.WriteLine($"Make: {vwMk3.Make}\nModel: {vwMk3.Model}\nYear: {vwMk3.Year}");
+        }
+    }
+}
