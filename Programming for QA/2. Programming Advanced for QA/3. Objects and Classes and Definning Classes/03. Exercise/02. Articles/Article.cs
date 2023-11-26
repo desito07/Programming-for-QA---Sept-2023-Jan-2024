50f573dbe822191b5d6c97ca2d4b266123a37a3c
Desi <d_andreeva82@yahoo.co.uk>
2023-11-26 22:35:16 +0000

    add file

---
+++ Programming for QA/2. Programming Advanced for QA/3. Objects and Classes and Definning Classes/03. Exercise/02. Articles/Article.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._Articles
+{
+    internal class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public Article( string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public void Edit( string newContent)
+        {
+            Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor )
+        {   
+            Author = newAuthor;
+
+        }
+
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+    }
+}
---
+++ Programming for QA/2. Programming Advanced for QA/3. Objects and Classes and Definning Classes/03. Exercise/02. Articles/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using _02._Articles;
+
+string articleData = Console.ReadLine();
+
+
+string title = articleData.Split(", ")[0];
+string content = articleData.Split(", ")[1];
+string author = articleData.Split(", ")[2];
+
+Article article = new Article(title, content, author);
+
+int countCommands = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < countCommands; i++)
+{
+    string command = Console.ReadLine();
+
+    string[] commandParts = command.Split(": ");
+    string commandName = commandParts[0];
+    string commandParameter = commandParts[1];
+
+    switch (commandName)
+    {
+        case "Edit":
+            article.Edit(commandParameter);
+                break;
+
+        case "ChangeAuthor":
+            article.ChangeAuthor(commandParameter);
+                break;
+        case "Rename":
+            article.Rename(commandParameter);
+                break;
+    }
+}
+
+Console.WriteLine($"{article.Title} - {article.Content} - {article.Author}");
