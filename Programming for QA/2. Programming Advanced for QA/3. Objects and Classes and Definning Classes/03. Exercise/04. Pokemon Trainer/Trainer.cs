beffd5966ca02dc5889cb0f51740a454c0bbf322
Desi <d_andreeva82@yahoo.co.uk>
2023-11-27 15:49:50 +0000

    add file

---
+++ Programming for QA/2. Programming Advanced for QA/3. Objects and Classes and Definning Classes/03. Exercise/04. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Pokemon_Trainer
+{
+    internal class Trainer
+    {
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public Trainer() { }
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.Badges = 0;
+            this.Pokemons = new List<Pokemon>();
+        }
+    }
+}
