b67da542d9da639f86d654a3a3e8ad67b7a057dc
Desi <d_andreeva82@yahoo.co.uk>
2023-11-30 22:57:36 +0000

    add file

---
+++ Programming for QA/2. Programming Advanced for QA/1. Strings and Text Processing and Regular Expressions/01. Exercise/03. Unit Test String Method Text Filter/TextFilterTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class TextFilterTests
+{
+    [Test]
+    public void Test_Filter_WhenNoBannedWords_ShouldReturnOriginalText()
+    {
+        // Arrange
+        string text = "Az iskam da polychavam po-dolqma zaplata!";
+        string[] banned = new[] { "kinti" };
+
+        // Act
+        string result = TextFilter.Filter(banned, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Az iskam da polychavam po-dolqma zaplata!"));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordExists_ShouldReplaceBannedWordWithAsterisks()
+    {
+        // Arrange
+        string text = "Az iskam da polychavam po-dolqma zaplata!";
+        string[] banned = new[] {"zaplata"};
+        string expected = "Az iskam da polychavam po-dolqma *******!";
+
+        // Act
+        string result = TextFilter.Filter(banned, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordsAreEmpty_ShouldReturnOriginalText()
+    {
+        // Arrange
+        string text = "Az iskam da polychavam po-dolqma zaplata!";
+        string[] banned = Array.Empty<string>();
+
+        // Act
+        string result = TextFilter.Filter(banned, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Az iskam da polychavam po-dolqma zaplata!"));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordsContainWhitespace_ShouldReplaceBannedWord()
+    {
+        // Arrange
+        string text = "Az iskam da polychavam po-dolqma zaplata!";
+        string[] banned = new[] { "zap la ta" };
+        string expected = "Az iskam da polychavam po-dolqma *******!";
+
+        // Act
+        string result = TextFilter.Filter(banned, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Az iskam da polychavam po-dolqma zaplata!"));
+    }
+}
