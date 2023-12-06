479cc82a867ada63016e1c535d1b22a2b3c160b9
Desi <d_andreeva82@yahoo.co.uk>
2023-12-06 22:47:38 +0000

    ADD FILE

---
+++ Programming for QA/2. Programming Advanced for QA/1. Strings and Text Processing and Regular Expressions/01. Exercise/05. Unit Test String Method Pattern/PatternTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class PatternTests
+{
+    [TestCase("abcdef", 2, "aBcDeFaBcDeF")]
+    [TestCase("ABCDEF", 1, "aBcDeF")]
+    [TestCase("abcdef", 10, "aBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeF")]
+    public void Test_GeneratePatternedString_ValidInput_ReturnsExpectedResult(string input, 
+        int repetitionFactor, string expected)
+    {
+        // Arrange
+
+        // Act
+        string result = Pattern.GeneratePatternedString(input, repetitionFactor);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_EmptyInput_ThrowsArgumentException()
+    {
+        //Arrange
+        string input = string.Empty;
+        int repetitionFactor = 10;
+
+        //Act & Assert
+        Assert.That(() => Pattern.GeneratePatternedString(input, repetitionFactor),
+                                                          Throws.ArgumentException);
+
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_NegativeRepetitionFactor_ThrowsArgumentException()
+    {
+        //Arrange
+        string input = "abc";
+        int repetitionFactor = -10;
+
+        //Act & Assert
+        Assert.That(() => Pattern.GeneratePatternedString(input, repetitionFactor),
+                                                          Throws.ArgumentException);
+
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_ZeroRepetitionFactor_ThrowsArgumentException()
+    {
+        //Arrange
+        string input = "abc";
+        int repetitionFactor = 0;
+
+        //Act & Assert
+        Assert.That(() => Pattern.GeneratePatternedString(input, repetitionFactor),
+                                                          Throws.ArgumentException);
+
+    }
+}
