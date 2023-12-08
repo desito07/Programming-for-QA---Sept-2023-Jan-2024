0fec1866f93f11dbe8eccb41b842bd4b96a8b993
Desi <d_andreeva82@yahoo.co.uk>
2023-12-08 17:17:53 +0000

    add file

---
+++ Programming for QA/2. Programming Advanced for QA/1. Strings and Text Processing and Regular Expressions/01. Exercise/10. Unit Test Regular Expression Email Validator/EmailValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailValidatorTests
+{
+    [TestCase("desi_andreeva@abv.bg")]
+    [TestCase("heyho@strat.co.uk")]
+    [TestCase("desi+test@unicursal.group")]
+    public void Test_ValidEmails_ReturnsTrue(string email)
+    {
+        // Arrange
+
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [TestCase("desi_^?=andreeva. @abv.bg")]
+    [TestCase("heyho@strat.co.u")]
+    [TestCase("desi+test-.unicursal.group")]
+    public void Test_InvalidEmails_ReturnsFalse(string email)
+    {
+        // Arrange
+        
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
