12d17c7530692121edf6b61210a6cff774cb033b
Desi <d_andreeva82@yahoo.co.uk>
2023-12-08 16:32:14 +0000

    Create MatchUrlsTests.cs

---
+++ Programming for QA/2. Programming Advanced for QA/1. Strings and Text Processing and Regular Expressions/01. Exercise/09. Unit Test RegEx Match URLs/MatchUrlsTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class MatchUrlsTests
+{
+    [Test]
+    public void Test_ExtractUrls_EmptyText_ReturnsEmptyList()
+    {
+        // Arrange
+        string text = "";
+        List<string> expected = new List<string>();
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+    [Test]
+    public void Test_ExtractUrls_NoUrlsInText_ReturnsEmptyList()
+    {
+        // Arrange
+        string text = "www.abv.g";
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_ExtractUrls_SingleUrlInText_ReturnsSingleUrl()
+    {
+        // Arrange
+        string text = "Here is my site: https://softuni.bg";
+        List<string> expected = new() { "https://softuni.bg" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_MultipleUrlsInText_ReturnsAllUrls()
+    {
+        // Arrange
+        string text = "Here are my websites: https://digital.softuni.bg, https://www.softuni.bg, some random text";
+        List<string> expected = new() { "https://digital.softuni.bg", "https://www.softuni.bg" };
+
+        // Act
+        var result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_UrlsInQuotationMarks_ReturnsUrlsInQuotationMarks()
+    {
+        // Arrange
+        string text = "Here is my website: \"https://digital.softuni.bg\"";
+        List<string> expected = new() { "https://digital.softuni.bg" };
+
+        // Act
+        var result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
