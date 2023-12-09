4f6d6d905ed9d32be772ee455bbad244f69c8dce
Desi <d_andreeva82@yahoo.co.uk>
2023-12-09 23:36:50 +0000

    add file

---
+++ Programming for QA/2. Programming Advanced for QA/2. Dictionaries, Lambda and LINQ/02. Lab/00. Demo.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+
+//Dictionary<string, string> phonebook = new();
+
+//phonebook.Add("Desi Andreeva", "02380123456");
+//phonebook.Add("Hey Ho Andreeva", "02392123478");
+//phonebook.Add("Desi Andreeva", "02380123456");
+
+//string value = phonebook["Desi Andreeva"];
+
+//Console.WriteLine(value);
+
+//phonebook["Desi Andreeva"] = "00000000000";
+
+//Console.WriteLine(phonebook["Desi Andreeva"]);
+
+//phonebook["Desi Andreeva"] = "123466789";
+
+//Console.WriteLine(phonebook["Desi Andreeva"]);
+
+//Console.WriteLine(phonebook.ContainsKey("HeyAndreeva"));
+
+/*var fruits = new SortedDictionary<string, double>();
+fruits["kiwi"] = 4.50;
+fruits["orange"] = 2.50;
+fruits["banana"] = 2.20;
+
+foreach (var fruit in fruits)
+{
+    Console.WriteLine($"{fruit.Key} {fruit.Value}");
+}
+
+Dictionary<string, double> hello = new ()
+{
+    {"hey", 2.5 },
+    {"ho", 5.2 },
+    {"ha", 8.5 }
+};*/
+
+/*Dictionary<string, int> planes = new();
+
+planes.Add("Airbus A320", 150);
+
+if(planes.ContainsKey("Airbus A320"))
+{
+    Console.WriteLine("yes");
+}*/
+
+/*List<int> testList = new() { 1, 2, 2, 5, 6};
+List<string> testListString = new() { "hello", "world", "Maybe?" };
+
+Console.WriteLine(testList.Min());
+Console.WriteLine(testListString.Min());
+
+Console.WriteLine(testList.Max());
+Console.WriteLine(testListString.Max());
+
+Console.WriteLine(testList.Sum());
+Console.WriteLine(testList.Average());*/
+
+
+List<string> testListString = new() { "hello", "world", "Maybe?" };
+
+List<string> names = new() { "Desi", "Teq", "Koko" };
+
+var result = names.Select(x => x + "!");
+Console.WriteLine(string.Join(", ", result));
+
+
+
---
+++ Programming for QA/2. Programming Advanced for QA/2. Dictionaries, Lambda and LINQ/02. Lab/02. Odd Occurrences.cs
@@ -0,0 +1,21 @@
+﻿string[] words = Console.ReadLine().Split(" ");
+
+Dictionary<string, int> wordsCount = new();
+
+foreach (string word in words)
+{
+    string caseInsensitiveWord = word.ToLower();
+    if (wordsCount.ContainsKey(caseInsensitiveWord))
+    {
+        wordsCount[caseInsensitiveWord]++;
+    }
+    else wordsCount.Add(caseInsensitiveWord, 1);
+}
+
+foreach(KeyValuePair<string, int>pair in wordsCount)
+{
+    if(pair.Value % 2 != 0)
+    {
+        Console.Write($"{pair.Key} ");
+    }
+}
