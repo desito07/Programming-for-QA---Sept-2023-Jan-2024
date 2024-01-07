e96daafed25d698597f5c92b25d01000e4014773
Desi <d_andreeva82@yahoo.co.uk>
2024-01-07 20:04:17 +0000

    add file

---
+++ Programming for QA/2. Programming Advanced for QA/4. Exception Handling/04. Lab/00. Demo.cs
@@ -0,0 +1,14 @@
+﻿string s = Console.ReadLine();
+
+try
+{
+    int.Parse(s);
+    Console.WriteLine("You entered a valid Int32 number {0}.", s);
+}
+catch(FormatException) {
+    Console.WriteLine("Invalid integer number!"); 
+}
+catch (OverflowException)
+{
+    Console.WriteLine("The number is too bog to fit in Int32!");
+}
